cac88d7c2eaf410a6dad345dd800fb40dcb13115
Shreyashtyagi <shreyashtyagi@example.com>
2026-08-02 13:17:13 +0530

    added new origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,8 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174"
+    "http://127.0.0.1:5174",
+    "https://nutrilog-ai-chi.vercel.app"
 ];
 
 app.use(cors({
